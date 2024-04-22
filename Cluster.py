correct the computation of clusters, compute isf from the check_point starting point to the end, adapt data_treatement, and don't compute check_step measurement everyon coarse step in Parallel_Run

Cluster.py
@@ -63,24 +63,25 @@ def cluster_points(points, max_distance):
 
     # Optional: Post-processing to merge overlapping clusters
     merged_clusters = []
-    while clusters:
-        first, rest = clusters[0], clusters[1:]
-        first = set(tuple(x) for x in first)  # Convert lists to sets of tuples for immutability
+    clusters_as_sets = [set(tuple(point) for point in cluster) for cluster in clusters] 
+    while clusters_as_sets:        
+        first, rest = clusters_as_sets[0], clusters_as_sets[1:]#clusters[0], clusters[1:]
+        #first = set(tuple(x) for x in first)  # Convert lists to sets of tuples for immutability
         merged = True
         while merged:
             merged = False
             for r in rest:
-                r_set = set(tuple(x) for x in r)
-                if not first.isdisjoint(r_set):  # Check if clusters overlap
-                    first |= r_set  # Union the sets
+                #r_set = set(tuple(x) for x in r)
+                if not first.isdisjoint(r):  # Check if clusters overlap
+                    first |= r  # Union the sets
                     rest.remove(r)
                     merged = True
                     break
         merged_clusters.append(list(first))
-        clusters = rest
+        clusters_as_sets = rest
 
     # Convert each cluster back to numpy arrays
-    return [np.array(list(map(list, cluster))) for cluster in merged_clusters]
+    return [np.array([list(point) for point in cluster]) for cluster in merged_clusters]#[np.array(list(map(list, cluster))) for cluster in merged_clusters]
 
 
 

Data_Treatement.py
@@ -121,7 +121,11 @@ class Data_Treatement:
             print(self.Read.get_measurement_data(self.Read.list_groups()[0],"Coarse_Time").shape)
             print(self.Read.get_measurement_data(self.Read.list_groups()[0],"Check_Time").shape)
             raise ValueError('No time with correct shape found')
-        self.time = np.array([self.Read.get_measurement_data(grp,TimeType) for grp in self.Read.list_groups() if self.Read.get_measurement_data(grp,TimeType) is not None],dtype=object)
+        if data_type !='ISF':
+            self.time = np.array([self.Read.get_measurement_data(grp,TimeType) for grp in self.Read.list_groups() if self.Read.get_measurement_data(grp,TimeType) is not None],dtype=object)
+        else:
+            coarse_time = np.array([self.Read.get_measurement_data(grp,"Coarse_Time") for grp in self.Read.list_groups() if self.Read.get_measurement_data(grp,"Coarse_Time") is not None],dtype=object)
+            self.time = [[t_sys[-isf.__len__():] - t_sys[-isf.__len__()] for isf in system]for  t_sys,system in zip(coarse_time,self.data)]
         self.Nsample = len(self.Read.list_groups())
         self.Read.close()
     def average(self,num_bins=100,log_scale=False):
@@ -145,7 +149,7 @@ class Data_Treatement:
         elif self.data_type in {'MSD','ISF'}:
             self.binned_time,self.average_data,self.variance = np.zeros((len(self.data[0]),num_bins),dtype=float),np.zeros((len(self.data[0]),num_bins),dtype=float),np.zeros((len(self.data[0]),num_bins),dtype=float)
             for index in range(len(self.data[0])):
-                times = np.array([self.time[i][index] for i in range(self.time.shape[0])])
+                times = np.array([self.time[i][index] for i in range(self.time.__len__())])
                 datas = np.array([self.data[i][index] for i in range(self.data.shape[0])])
                 self.binned_time[index],self.average_data[index],self.variance[index] = average_scalar(times,datas,num_bins=num_bins,log_scale=log_scale)
                 self.average_data[index] = interpolate_empty_bins(self.average_data[index])

ISF.py
@@ -33,18 +33,29 @@ class ISF:
             self.q_vectors = q_norm * uniform_sphere_samples(num_q_samples)            
             self.gillespie = gillespie
             #self.isf_time = np.zeros((step_tot//check_steps,check_steps//coarse_grained_step),dtype=float)
-            self.isf_time = [np.zeros((end-start)//coarse_grained_steps, dtype=float) for start, end in zip([0]+log_check_points[:-1], log_check_points)]
+            #self.isf_time = [np.zeros((end-start)//coarse_grained_steps, dtype=float) for start, end in zip([0]+log_check_points[:-1], log_check_points)]
+            self.isf_time = [np.zeros((step_tot-initial_step)//coarse_grained_steps,dtype=float) for initial_step in [0]+log_check_points[:-1]]
+            self.initial_positions = np.zeros((log_check_points.__len__(),gillespie.Nlinker,3),dtype=float)  # one initial position per isf
+            self.next_index = 0 # index of the next isf to be computed
+            self.next_time=np.zeros((log_check_points.__len__()),dtype=int)  # adjust the index for each isf
         def compute(self,time,move,i,t,*args):
             try:
-                self.isf_time[i][t] = np.linalg.norm(Compute_Average_ISF(self.gillespie.get_r(periodic=True),self.initial_positions,self.q_vectors))
+                #self.isf_time[i][t] = np.linalg.norm(Compute_Average_ISF(self.gillespie.get_r(periodic=True),self.initial_positions,self.q_vectors))
+                for n in range(self.next_index):
+                    self.isf_time[n][self.next_time[n]] = np.linalg.norm(Compute_Average_ISF(self.gillespie.get_r(periodic=True),self.initial_positions[n],self.q_vectors))
+                    self.next_time[n]+=1
             except IndexError:
                 print(i)
                 print(t)
+                print(self.next_time)
                 print(len(self.isf_time))
                 print(self.isf_time[i].shape)
-                raise
+                #raise
         def start_check_step(self,*args):
-            self.initial_positions = copy.copy(self.gillespie.get_r(periodic=True))
+            #self.initial_positions = copy.copy(self.gillespie.get_r(periodic=True))
+            self.initial_positions[self.next_index] = copy.copy(self.gillespie.get_r(periodic=True))
+            self.next_index+=1
+            #print(self.next_time)
         def end_check_step(self,*args):
             return
         def close(self,output,*args):