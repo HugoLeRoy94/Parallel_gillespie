2d49e487b241ddf75be6522708122a43ed04b46b
cachetdaspirine <leroyhugo@yahoo.fr>
2024-06-17 13:50:57 +0200

    bug_correction: no dist matrix in pcf

--- PCF.py
+++ PCF.py
@@ -23,16 +23,16 @@ class PCF:
     def compute(self,time,move,*args):
         self.time+=np.sum(time)
         self.counts+=self.prev_hist*time[0]
-        dist_matrix = distance_matrix(self.gillespie.get_r(),self.gillespie.get_r())
+        self.dist_matrix = distance_matrix(self.gillespie.get_r(),self.gillespie.get_r())
         #self.dist = dist_matrix[np.triu_indices_from(dist_matrix, k=1)]
         # We need to ignore the self-distances by setting the diagonal to infinity
-        np.fill_diagonal(dist_matrix, np.inf)
+        np.fill_diagonal(self.dist_matrix, np.inf)
         # Find the nearest neighbor distances
-        nearest_neighbor_distances = np.min(dist_matrix, axis=1)
+        nearest_neighbor_distances = np.min(self.dist_matrix, axis=1)
         #self.prev_hist, bin_edges = histogram_float(self.dist, bins=self.num_bins, range=(0, self.max_distance))
         self.prev_hist, _ = histogram_float(nearest_neighbor_distances, bins=self.bin_edges, range=(self.bin_edges[0], self.bin_edges[-1]))
     def end_check_step(self,i,*args):
-        self.counts = self.counts / (self.time * self.shell_volumes*self.dist.shape[0])
+        self.counts = self.counts / (self.time * self.shell_volumes*self.dist_matrix.shape[0])
         self.t_tot+=self.time
         self.PCF_counts[i] = np.stack((self.bin_centers,self.counts), axis=-1)
     def close(self,output,*args):
