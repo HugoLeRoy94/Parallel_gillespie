db63ff713ca258c174f2b9d2f597e375e397fbf9
cachetdaspirine <leroyhugo@yahoo.fr>
2024-06-17 13:42:22 +0200

    compute the nearest neighbor probability distribution

--- PCF.py
+++ PCF.py
@@ -3,6 +3,7 @@ from scipy.spatial import distance_matrix
 def histogram_float(*args, **kwargs):
     counts, bin_edges = np.histogram(*args, **kwargs)
     return counts.astype(float), bin_edges
+
 class PCF:
     def __init__(self,step_tot,log_check_points,coarse_grained_steps,gillespie,max_distance,num_bins,*args):
         self.num_bins = num_bins
@@ -23,9 +24,13 @@ class PCF:
         self.time+=np.sum(time)
         self.counts+=self.prev_hist*time[0]
         dist_matrix = distance_matrix(self.gillespie.get_r(),self.gillespie.get_r())
-        self.dist = dist_matrix[np.triu_indices_from(dist_matrix, k=1)]
+        #self.dist = dist_matrix[np.triu_indices_from(dist_matrix, k=1)]
+        # We need to ignore the self-distances by setting the diagonal to infinity
+        np.fill_diagonal(dist_matrix, np.inf)
+        # Find the nearest neighbor distances
+        nearest_neighbor_distances = np.min(dist_matrix, axis=1)
         #self.prev_hist, bin_edges = histogram_float(self.dist, bins=self.num_bins, range=(0, self.max_distance))
-        self.prev_hist, _ = histogram_float(self.dist, bins=self.bin_edges, range=(self.bin_edges[0], self.bin_edges[-1]))
+        self.prev_hist, _ = histogram_float(nearest_neighbor_distances, bins=self.bin_edges, range=(self.bin_edges[0], self.bin_edges[-1]))
     def end_check_step(self,i,*args):
         self.counts = self.counts / (self.time * self.shell_volumes*self.dist.shape[0])
         self.t_tot+=self.time
